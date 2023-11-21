022e8ac0c088d9555f39b56fa845752211c788de
Ricardo-609 <673298654@qq.com>
2023-11-21 17:35:05 +0800

    框架

--- app/src/main/java/com/ricardo/learn_camerax/MainActivity.java
+++ app/src/main/java/com/ricardo/learn_camerax/MainActivity.java
@@ -1,14 +1,79 @@
 package com.ricardo.learn_camerax;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageCapture;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageCapture imageCapture;
+    private File outputDirectory;
+    private ExecutorService cameraExecutor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 请求相机权限
+        if (allPermissionGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, Configuration.REQUIRED_PERMISSIONS,
+                    Configuration.REQUEST_CODE_PERMISSIONS);
+        }
+
+        // 设置拍照按钮监听
+        Button camera_capture_button = findViewById(R.id.camera_capture_button);
+        camera_capture_button.setOnClickListener(v -> takePhoto());
+
+        // 设置照片保存位置
+        outputDirectory = getOutputDirectory();
+        cameraExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
+
+    private void startCamera() {}
+
+    private void takePhoto() {}
+
+    private File getOutputDirectory() {
+        File mediaDir = new File(getExternalMediaDirs()[0], getString(R.string.app_name));
+        boolean isExist = mediaDir.exists() || mediaDir.mkdir();
+        return isExist ? mediaDir : null;
+    }
+
+    private boolean allPermissionGranted() {
+        for (String permission : Configuration.REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static class Configuration {
+        public static final String TAG = "learn_camerax";
+        public static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+        public static final int REQUEST_CODE_PERMISSIONS = 10;
+        public static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
     }
 }
