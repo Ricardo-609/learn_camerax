ba4dd306a1bc15ebbb103ac829a0970bc5946b95
Ricardo-609 <673298654@qq.com>
2023-11-21 18:24:43 +0800

    实现拍照用例

--- app/src/main/java/com/ricardo/learn_camerax/MainActivity.java
+++ app/src/main/java/com/ricardo/learn_camerax/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ricardo.learn_camerax;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -12,6 +14,7 @@ import androidx.core.content.ContextCompat;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -20,10 +23,16 @@ import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import android.Manifest;
+import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.util.Locale.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -75,17 +84,61 @@ public class MainActivity extends AppCompatActivity {
                 preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
 
                 CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                // 创建拍照所需的实例
+                imageCapture = new ImageCapture.Builder().build();
+
+
                 // 重新绑定用例前先解绑
                 processCameraProvider.unbindAll();
                 // 绑定用例至相机
-                processCameraProvider.bindToLifecycle(MainActivity.this, cameraSelector, preview);
+//                processCameraProvider.bindToLifecycle(MainActivity.this, cameraSelector, preview);
+
+                // 绑定拍照用例
+                processCameraProvider.bindToLifecycle(MainActivity.this, cameraSelector,
+                        preview,
+                        imageCapture);
             } catch (Exception e) {
                 Log.e(Configuration.TAG, "绑定失败");
             }
         }, ContextCompat.getMainExecutor(this));
+
     }
 
-    private void takePhoto() {}
+    private void takePhoto() {
+        // 确保imageCapture 已经被实例化, 否则程序将可能崩溃
+        if (imageCapture != null) {
+            // 创建带时间戳的输出文件以保存图片，带时间戳是为了保证文件名唯一
+            File photoFile = new File(outputDirectory,
+                    new SimpleDateFormat(Configuration.FILENAME_FORMAT,
+                            SIMPLIFIED_CHINESE).format(System.currentTimeMillis())
+                            + ".jpg");
+
+            // 创建 output option 对象，用以指定照片的输出方式
+            ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions
+                    .Builder(photoFile)
+                    .build();
+
+            // 执行takePicture（拍照）方法
+            imageCapture.takePicture(outputFileOptions,
+                    ContextCompat.getMainExecutor(this),
+                    new ImageCapture.OnImageSavedCallback() {       // 保存照片时的回调
+                        @Override
+                        public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                            Uri savedUri = Uri.fromFile(photoFile);
+                            String msg = "照片捕获成功! " + savedUri;
+                            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                            Log.d(Configuration.TAG, msg);
+                        }
+
+                        @Override
+                        public void onError(@NonNull ImageCaptureException exception) {
+                            Log.e(Configuration.TAG, "Photo capture failed: " + exception.getMessage());
+
+                        }
+                    });
+        }
+    }
 
     private File getOutputDirectory() {
         File mediaDir = new File(getExternalMediaDirs()[0], getString(R.string.app_name));
